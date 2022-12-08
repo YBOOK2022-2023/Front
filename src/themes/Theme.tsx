3e6259541ede92c0d673499431d7a0fdd4552708
Nydwhal <cyril.cauquil@gmail.com>
2022-12-08 10:42:43 +0100

    main-layout

---
+++ src/themes/Theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const outerTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#bd80fb",
+    },
+  },
+});
